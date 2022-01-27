f88bfb57c94d7367b91f13d5fb49a604ba1434b0
Mislav Hećimović <mislav.hecimovic@gmail.com>
2022-01-27 15:28:43 +0100

    BehaviorSubject, Observable, OnPush changedetection example

--- front/src/app/components/todo/todo.component.ts
+++ front/src/app/components/todo/todo.component.ts
@@ -1,22 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import {ListToDo} from '../../mock-ToDo';
 import {ToDo} from '../../ToDo';
 import {TodoService} from '../../services/todo.service';
-import { Subscriber } from 'rxjs';
+import { Observable, Subscriber } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
-  styleUrls: ['./todo.component.css']
+  styleUrls: ['./todo.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class TodoComponent implements OnInit {
-  listtodo: ToDo[]= [];
+
+  listtodo: Observable<ToDo[]> = new Observable<ToDo[]>();
 
   constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
-    this.todoService.getListToDo().subscribe((listtodo) => this.listtodo=listtodo );
+    this.listtodo = this.todoService.getListToDo();
   }
-
 }
--- front/src/app/services/todo.service.ts
+++ front/src/app/services/todo.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable, observable, of} from 'rxjs';
+import { BehaviorSubject, Observable, observable, of } from 'rxjs';
 import { ListToDo } from '../mock-ToDo';
 import { ToDo } from '../ToDo';
 
@@ -14,11 +14,24 @@ export class TodoService {
   readonly apiURL='https://localhost:7221/todoitems';
   formData: ToDo = new ToDo();
 
+  private todos = new BehaviorSubject<ToDo[]>(ListToDo);
+  private todos$ = this.todos.asObservable();
+
   getListToDo(): Observable<ToDo[]>{
    // return this.http.get<ToDo[]>(this.apiURL);
-    
-    const Listtodo= of(ListToDo);
-    return Listtodo;
-    
+
+    // const Listtodo= of(ListToDo);
+    // return Listtodo;
+
+    return this.todos$;
+  }
+
+  metoda() {
+    this.todos.next([{
+      id: 100,
+      name: 'ToDo taks number 1',
+      day: 'Feb 4th at 14:30',
+      reminder: true,
+    }])
   }
 }
