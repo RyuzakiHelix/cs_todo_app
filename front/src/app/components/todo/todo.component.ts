8974ef569ec2963b9859f0c832eafb06dd49a098
Ivan Blažek <ryuzakihelix@gmail.com>
2022-03-13 13:25:25 +0100

    feat: made a function to count reminders
    optional: make a counter component separate from todo

--- front/src/app/components/todo/todo.component.ts
+++ front/src/app/components/todo/todo.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
 import {ListToDo} from '../../mock-ToDo';
 import {ToDo} from '../../ToDo';
 import {TodoService} from '../../services/todo.service';
-import { Subscriber, Observable } from 'rxjs';
+import { Subscriber, Observable, Subscription } from 'rxjs';
 import { HttpClientModule } from '@angular/common/http';
 
 @Component({
@@ -13,25 +13,55 @@ import { HttpClientModule } from '@angular/common/http';
 export class TodoComponent implements OnInit {
   //listtodo: ToDo[]= [];
   listtodo: Observable<ToDo[]> = new Observable<ToDo[]>();
+ // all :Observable<number> = new Observable<number>();
+  all : number=0;
+  total : number=0;
+  sub : Subscription = new Subscription;
 
   constructor(private todoService: TodoService) { }
 
   ngOnInit(): void {
     //this.todoService.getListToDo().subscribe((listtodo) => this.listtodo=listtodo );
     this.listtodo = this.todoService.getListToDo();
+    this.count();
+   // this.all = this.todoService.ReminderCounter();
   }
 
+  count(){
+    this.sub = this.todoService.getListToDo().subscribe(res=>{
+      this.all= this.todoService.ReminderCounter();
+      this.total = res.length;
+    });
+  }
+  
   deleteTodo(todo:ToDo){
     console.log("delete");
     console.log(todo);
-    this.todoService.deleteToDo(todo).subscribe(() => this.listtodo = this.todoService.getListToDo());
+    this.todoService.deleteToDo(todo).subscribe(() => {
+      this.listtodo = this.todoService.getListToDo();
+
+      this.sub = this.todoService.getListToDo().subscribe(res=>{
+        this.all= this.todoService.ReminderCounter();
+        this.total = res.length;
+      });
+
+    });
+   // this.count();
 
   }
   addToDo(todo:ToDo){
     console.log("add");
     console.log(todo);
     //this.todoService.addToDo(todo).subscribe((todo) => this.listtodo.push(todo));
-    this.todoService.addToDo(todo).subscribe(() => this.listtodo = this.todoService.getListToDo());
+    this.todoService.addToDo(todo).subscribe(() => {
+      this.listtodo = this.todoService.getListToDo();
+
+      this.sub = this.todoService.getListToDo().subscribe(res=>{
+        this.all= this.todoService.ReminderCounter();
+        this.total = res.length;
+      });
+    });
+    //this.count();
   }
   
   toggleTodo(todo:ToDo){
@@ -39,6 +69,7 @@ export class TodoComponent implements OnInit {
     console.log("toggle");
     console.log(todo);
     this.todoService.toggleToDo(todo).subscribe();
+    this.all=this.todoService.ReminderCounter();
 
   }
 
--- front/src/app/services/todo.service.ts
+++ front/src/app/services/todo.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import {Observable, BehaviorSubject, tap} from 'rxjs';
+import {Observable, BehaviorSubject, tap, filter, count, of, Subject, map, lastValueFrom} from 'rxjs';
 import { ToDo } from '../ToDo';
 
 const httpOptions ={
@@ -27,15 +27,64 @@ export class TodoService {
   private todos$ = this.todos.asObservable();
 
   getListToDo(): Observable<ToDo[]>{
-   // return this.http.get<ToDo[]>(this.apiURL);
     return this.http.get<ToDo[]>(this.apiURL).pipe(
       tap(response =>{
         this.todos.next(response)
       })
-    );
-      
-   //return this.todos$=this.http.get<ToDo[]>(this.apiURL);
-   // return this.todos$;
+    ); 
+  }
+  
+  ReminderCounter():number{
+    var count=0;
+    this.todos.subscribe(result => {console.log('total el ',result.length)});
+    this.todos.subscribe(result => {
+      result.forEach(todo=>{
+        count += todo.reminder ? 1:0;
+      })
+      console.log('el set reminder ',count);
+    });
+    
+    return count;
+  }
+/*
+  ReminderCounter(){
+    var count=0;
+    this.ListToDo.forEach(element => {
+      count += element.reminder ? 0:1;
+      console.log(element);
+    });
+    console.log(count);
+    return count;
+  }
+  */
+   
+  countListToDoReminders(): number{
+    var sum=0;
+    this.todos$.subscribe(res =>{
+     // console.log(res.filter(t=>t.reminder===true));
+     // console.log('ukupno ',res.length);
+      sum=res.length;
+    });
+
+    var all = this.todos.pipe(count());
+    all.subscribe(val=>console.log('ukupno '+ val));
+    this.todos.subscribe(res=>{res.filter(t=>t.reminder===true)});
+
+    let all_nums = of(this.todos$);
+    let final_val = all_nums.pipe(count());
+    final_val.subscribe(x => console.log("The count is "+x));
+    
+    
+    
+   // console.log(this.ListToDo.filter(t=>t.reminder===true).length);
+   // let total = 0;
+   // console.log(this.todos.forEach(value => {
+     // total += 1;
+    //  console.log('sveukupnoih ima: ', total, value);
+   // }))
+    return sum;
+   // return this.ListToDo.filter(t=>t.reminder===true).length;
+
   }
 
   deleteToDo(todo:ToDo): Observable<ToDo>{
